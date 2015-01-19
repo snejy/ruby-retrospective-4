7c0e43555f3927a89ec45d493eb43bcf00540a5d
snejy <snspasova@gmail.com>
2015-01-19 11:29:46 +0200

    refactor solution to third task

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,90 +1,90 @@
 module RBFS
   class File
-    attr_reader :data_type
     attr_accessor :data
 
     def initialize(data = nil)
-        @data = data
-        @data_type = get_data_type
+      @data = data
     end
 
-    def get_data_type
+    def data_type
       case @data
-      when Numeric then :number
-      when String then :string
-      when Symbol then :symbol
-      when !!@data then :boolean
+      when Fixnum, Float         then :number
+      when String                then :string
+      when Symbol                then :symbol
+      when TrueClass, FalseClass then :boolean
       else :nil
       end
     end
 
-    def self.to_num(string_number)
-      if string_number.include? '.'
-      then string_number.to_f
-      else string_number.to_i
-      end
-    end
-
-    def self.to_bool(string_boolean)
-      string_boolean == 'true'
-    end
-
-    def self.data_from_type(string_data_type, string_data)
-      case string_data
-      when "number" then self.to_num(string_data)
-      when "symbol" then string_data.to_sym
-      when "boolean" then self.to_bool(string_data_type)
-      when "nil" then nil
-      else string_data
-      end
-    end
-
     def serialize
-      @data_type = get_data_type
-      "#{@data_type}:#{@data}"
+      "#{data_type}:#{@data}"
     end
 
     def self.parse(object)
-      type, data = object.split(':')
-      self.new (self.data_from_type(data, type))
+      type, data = object.split(':', 2)
+      data = case type
+             when 'string'  then data
+             when 'symbol'  then data.to_sym
+             when 'number'  then data.to_f
+             when 'boolean' then data == 'true'
+             else nil
+             end
+      self.new (data)
     end
   end
 
   class Directory
-    attr_reader :files
+    attr_reader :files, :directories
 
     def initialize
-      @hierarchy = {}
-    end
-
-    def files
-      @hierarchy.select { |key, value| value.instance_of? RBFS::File }
-    end
-
-    def directories
-       @hierarchy.select { |key, value| value.instance_of? RBFS::Directory }
+      @files = {}
+      @directories = {}
     end
 
     def add_file(name, file)
-      @hierarchy[name] = file unless name.include? ":"
+      @files[name] = file
     end
 
-    def add_directory(name, direcotry = RBFS::Directory.new)
-      @hierarchy[name] = direcotry unless name.include? ":"
+    def add_directory(name, directory = Directory.new)
+      @directories[name] = directory
     end
 
     def [](name)
-      @hierarchy[name]
+      @directories[name] || @files[name]
     end
 
     def serialize
-      "#{self.files.size}:" + self.files.map { |name, file|
+      "#{files.size}:" + files.map { |name, file|
       "#{name}:#{file.serialize.length}:#{file.serialize}" }.join +
-      "#{self.directories.size}:" + self.directories.map {|name, dir|
-      "#{name}:#{dir.serialize.length}:#{dir.serialize}"}.join
+      "#{directories.size}:" + directories.map {|name, directory|
+      "#{name}:#{directory.serialize.length}:#{directory.serialize}"}.join
     end
 
-    def self.parse(object)
+    def self.parse(string)
+      directory = Directory.new
+      parser = Parser.new(string)
+      parser.parse do |name, data|
+        directory.add_file(name, File.parse(data))
+      end
+      parser.parse do |name, data|
+        directory.add_directory(name, Directory.parse(data))
+      end
+      directory
+    end
+  end
+
+  class Parser
+    def initialize(string)
+      @data = string
+    end
+
+    def parse
+      count, @data = @data.split(":", 2)
+      count.to_i.times do
+        name, length, left = @data.split(":", 3)
+        yield name, left[0...length.to_i]
+        @data = left[length.to_i..-1]
+      end
     end
   end
 end
